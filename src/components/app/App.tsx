2c31edd0ed4a2a53912f69ed73b603f2ce23e33a
Aleksandr <it.kucherov@gmail.com>
2022-10-13 15:13:35 +0300

    start new project

---
+++ src/components/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './app.module.scss';
+
+function App() {
+	return (
+		<div className="App">
+			<h1>HELLO SPORT MANAGER APP</h1>
+		</div>
+	);
+}
+
+export default App;
