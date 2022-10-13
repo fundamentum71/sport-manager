2a086cc12aeac1f2f34fdd08fd12e40068b0d84a
Aleksandr <it.kucherov@gmail.com>
2022-10-13 18:51:48 +0300

    первые наброски

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import StartPage from '../../pages/StartPage';
+import Header from '../header/Header';
 
-import styles from './app.module.scss';
+//import styles from './app.module.scss';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>HELLO SPORT MANAGER APP</h1>
+			<Header />
+			<StartPage />
 		</div>
 	);
 }
