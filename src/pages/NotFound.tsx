5fec2ed07f3d5dbabd9e76598690915b76185115
Aleksandr <it.kucherov@gmail.com>
2022-10-28 15:27:24 +0300

    сделал notfound

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './notFound.module.scss';
 
 function NotFound() {
-	return <div>NotFound</div>;
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.error}>
+				<p>Ошибка 404</p>
+				<p>Страница не найдена</p>
+			</div>
+			<Link to={'/'}>
+				<p className={styles.back}>Вернуться на главную</p>
+			</Link>
+		</div>
+	);
 }
 
 export default NotFound;
