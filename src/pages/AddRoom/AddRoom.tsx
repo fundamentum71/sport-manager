ddb3289b4d705a14df5869df1b926abaa93333f5
Aleksandr <it.kucherov@gmail.com>
2022-11-02 14:31:58 +0300

    создание комнаты

--- src/pages/AddRoom/AddRoom.tsx
+++ src/pages/AddRoom/AddRoom.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
+import styles from './addRoom.module.scss';
 
 const AddRoom = () => {
 	return (
-		<div>
-			<label htmlFor=""> Назваине комнаты</label>
-			<input type="text" placeholder="Введите название комнаты" />
-			<label htmlFor="">Дата</label>
-			<input type="date" placeholder="" />
-			<label htmlFor="">Время</label>
-			<input type="time" />
-			<label htmlFor="">Площадка</label>
-			<input type="text" placeholder="Введите адрес" />
-			<button>Создать комнату</button>
-		</div>
+		<section className={styles.wrapper}>
+			<div>
+				<h2>Создание комнаты</h2>
+				<button className={styles.btn_red}>Удалить комнату</button>
+			</div>
+			<div className={styles.options}>
+				<label htmlFor=""> Название комнаты</label>
+				<input type="text" placeholder="Введите название комнаты" />
+				<label htmlFor="">Вид спорта</label>
+				<input type="text" placeholder="Вид спорта" />
+				<label htmlFor="">Дата</label>
+				<input type="date" placeholder="" />
+				<label htmlFor="">Время</label>
+				<input type="time" />
+				<label htmlFor="">Площадка</label>
+				<input type="text" placeholder="Введите адрес" />
+				<button className={styles.btn}>Создать комнату</button>
+			</div>
+		</section>
 	);
 };
 
