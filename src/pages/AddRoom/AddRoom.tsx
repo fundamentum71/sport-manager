72bd4b9903c8e113e302ab917a5f2a1d299858dd
Aleksandr <it.kucherov@gmail.com>
2022-11-02 11:58:44 +0300

    набросал комнату

--- src/pages/AddRoom/AddRoom.tsx
+++ src/pages/AddRoom/AddRoom.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const AddRoom = () => {
-	return <div>AddRoom</div>;
+	return (
+		<div>
+			<label htmlFor=""> Назваине комнаты</label>
+			<input type="text" placeholder="Введите название комнаты" />
+			<label htmlFor="">Дата</label>
+			<input type="date" placeholder="" />
+			<label htmlFor="">Время</label>
+			<input type="time" />
+			<label htmlFor="">Площадка</label>
+			<input type="text" placeholder="Введите адрес" />
+			<button>Создать комнату</button>
+		</div>
+	);
 };
 
 export default AddRoom;
