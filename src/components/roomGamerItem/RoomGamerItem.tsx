6bb46cbedb1db901fbdc04b7a2cd9375511ca009
Aleksandr <it.kucherov@gmail.com>
2022-11-26 12:22:58 +0300

    стилизовал список участников комнаты

--- src/components/roomGamerItem/RoomGamerItem.tsx
+++ src/components/roomGamerItem/RoomGamerItem.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './roomGamersItem.module.scss';
+import avatar from '../../assets/images/avatar.jpg';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 
 type RoomGamerItem = {
 	email?: string;
@@ -21,5 +23,37 @@ export const RoomGamerItem: React.FC<RoomGamerItem> = ({
 	gamesPlayed,
 	gamesLeave,
 }) => {
-	return <div className={styles.wrapper}>{fullName}</div>;
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.line}>
+				<div className={styles.avatar}>
+					<img src={avatar} alt="avatar" />
+				</div>
+				<div>{fullName}</div>
+				<InfoOutlinedIcon className={styles.info_icon} />
+			</div>
+			<div className={styles.slide_info}>
+				<p>
+					<b>id: </b>
+					{_id}
+				</p>
+				<p>
+					<b>Почта: </b>
+					{email}
+				</p>
+				<p>
+					<b>Возраст: </b>
+					{age}
+				</p>
+				<p>
+					<b>Сыграно игр: </b>
+					{gamesPlayed}
+				</p>
+				<p>
+					<b>Не явился: </b>
+					{gamesLeave}
+				</p>
+			</div>
+		</div>
+	);
 };
